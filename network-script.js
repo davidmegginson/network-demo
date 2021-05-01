92d81cb8b44a5959b4f53d48db1c04e8b7348b2f
David Megginson <david.megginson@gmail.com>
2021-05-01 15:33:22 -0400

    Removed old static data.

--- network-script.js
+++ network-script.js
@@ -12,7 +12,7 @@ function run_simulation (nodes_data, links_data) {
         .nodes(nodes_data);
 
     var link_force =  d3.forceLink(links_data)
-        .id(function(d) { return d.stub; });            
+        .id(d => d.stub);            
 
     var charge_force = d3.forceManyBody()
         .strength(-100); 
@@ -39,7 +39,7 @@ function run_simulation (nodes_data, links_data) {
         .selectAll("line")
         .data(links_data)
         .enter().append("line")
-        .attr("stroke-width", 2)
+        .attr("stroke-width", d => d.value)
         .style("stroke", linkColour);        
 
     //draw circles for the nodes 
@@ -54,7 +54,7 @@ function run_simulation (nodes_data, links_data) {
 
     // Show full name on mouseover
     node.append("title")
-        .text(function(d) { return d.name; });
+        .text(d => d.name);
 
     //add drag capabilities  
     var drag_handler = d3.drag()
@@ -136,87 +136,12 @@ function run_simulation (nodes_data, links_data) {
     }
 }
 
-var nodes_data =  [
-    {"name": "Lillian", "sex": "F"},
-    {"name": "Gordon", "sex": "M"},
-    {"name": "Sylvester", "sex": "M"},
-    {"name": "Mary", "sex": "F"},
-    {"name": "Helen", "sex": "F"},
-    {"name": "Jamie", "sex": "M"},
-    {"name": "Jessie", "sex": "F"},
-    {"name": "Ashton", "sex": "M"},
-    {"name": "Duncan", "sex": "M"},
-    {"name": "Evette", "sex": "F"},
-    {"name": "Mauer", "sex": "M"},
-    {"name": "Fray", "sex": "F"},
-    {"name": "Duke", "sex": "M"},
-    {"name": "Baron", "sex": "M"},
-    {"name": "Infante", "sex": "M"},
-    {"name": "Percy", "sex": "M"},
-    {"name": "Cynthia", "sex": "F"},
-    {"name": "Feyton", "sex": "M"},
-    {"name": "Lesley", "sex": "F"},
-    {"name": "Yvette", "sex": "F"},
-    {"name": "Maria", "sex": "F"},
-    {"name": "Lexy", "sex": "F"},
-    {"name": "Peter", "sex": "M"},
-    {"name": "Ashley", "sex": "F"},
-    {"name": "Finkler", "sex": "M"},
-    {"name": "Damo", "sex": "M"},
-    {"name": "Imogen", "sex": "F"}
-]
 
-//Sample links data 
-//type: A for Ally, E for Enemy
-var links_data = [
-    {"source": "Sylvester", "target": "Gordon", "type":"A" },
-    {"source": "Sylvester", "target": "Lillian", "type":"A" },
-    {"source": "Sylvester", "target": "Mary", "type":"A"},
-    {"source": "Sylvester", "target": "Jamie", "type":"A"},
-    {"source": "Sylvester", "target": "Jessie", "type":"A"},
-    {"source": "Sylvester", "target": "Helen", "type":"A"},
-    {"source": "Helen", "target": "Gordon", "type":"A"},
-    {"source": "Mary", "target": "Lillian", "type":"A"},
-    {"source": "Ashton", "target": "Mary", "type":"A"},
-    {"source": "Duncan", "target": "Jamie", "type":"A"},
-    {"source": "Gordon", "target": "Jessie", "type":"A"},
-    {"source": "Sylvester", "target": "Fray", "type":"E"},
-    {"source": "Fray", "target": "Mauer", "type":"A"},
-    {"source": "Fray", "target": "Cynthia", "type":"A"},
-    {"source": "Fray", "target": "Percy", "type":"A"},
-    {"source": "Percy", "target": "Cynthia", "type":"A"},
-    {"source": "Infante", "target": "Duke", "type":"A"},
-    {"source": "Duke", "target": "Gordon", "type":"A"},
-    {"source": "Duke", "target": "Sylvester", "type":"A"},
-    {"source": "Baron", "target": "Duke", "type":"A"},
-    {"source": "Baron", "target": "Sylvester", "type":"E"},
-    {"source": "Evette", "target": "Sylvester", "type":"E"},
-    {"source": "Cynthia", "target": "Sylvester", "type":"E"},
-    {"source": "Cynthia", "target": "Jamie", "type":"E"},
-    {"source": "Mauer", "target": "Jessie", "type":"E"},
-    {"source": "Duke", "target": "Lexy", "type":"A"},
-    {"source": "Feyton", "target": "Lexy", "type":"A"},
-    {"source": "Maria", "target": "Feyton", "type":"A"},
-    {"source": "Baron", "target": "Yvette", "type":"E"},
-    {"source": "Evette", "target": "Maria", "type":"E"},
-    {"source": "Cynthia", "target": "Yvette", "type":"E"},
-    {"source": "Maria", "target": "Jamie", "type":"E"},
-    {"source": "Maria", "target": "Lesley", "type":"E"},
-    {"source": "Ashley", "target": "Damo", "type":"A"},
-    {"source": "Damo", "target": "Lexy", "type":"A"},
-    {"source": "Maria", "target": "Feyton", "type":"A"},
-    {"source": "Finkler", "target": "Ashley", "type":"E"},
-    {"source": "Sylvester", "target": "Maria", "type":"E"},
-    {"source": "Peter", "target": "Finkler", "type":"E"},
-    {"source": "Ashley", "target": "Gordon", "type":"E"},
-    {"source": "Maria", "target": "Imogen", "type":"E"}
-]
-
+// Load JSON then render
 let promise = fetch("files/network.json");
 
 promise.then(result => {
     result.json().then(data => {
-        console.log(data);
         run_simulation(data.nodes, data.links);
     });
 });
